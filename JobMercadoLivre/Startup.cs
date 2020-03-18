9d06232dc14468b6a7ab48f6e2517f0d13638260
LeonardoSena1 <57286132+LeonardoSena1@users.noreply.github.com>
2020-03-18 13:40:11 -0300

    Hangifre abertro

--- JobMercadoLivre/Startup.cs
+++ JobMercadoLivre/Startup.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Hangfire;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -22,6 +19,8 @@ namespace JobMercadoLivre
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHangfire(x => x.UseSqlServerStorage("Data Source =.\\SQLEXPRESS; Initial Catalog = HangfireTest; Integrated Security = True;"));
+            services.AddHangfireServer();
             services.AddControllersWithViews();
         }
 
@@ -37,6 +36,8 @@ namespace JobMercadoLivre
                 app.UseExceptionHandler("/Home/Error");
             }
             app.UseStaticFiles();
+
+            app.UseHangfireDashboard();
 
             app.UseRouting();
 
